1abee3a50346b206457ef66f4d6bf0a3ddcbeb63
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2023-12-28 23:59:20 +0000

    Add snackbar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Snackbar from "./teabreak/snackbar";
 import Tooltip from "./teabreak/tooltip";
 
 //Tooltip
@@ -5,4 +6,13 @@ const tooltips = document.querySelectorAll(".tooltip");
 tooltips.forEach((tooltip) => {
   const t = new Tooltip(tooltip);
   t.init();
+});
+
+//Snackbar
+const snackbar = new Snackbar(document.querySelector("snackbar-trigger"));
+snackbar.init();
+
+const link = document.querySelector("#snack");
+link.addEventListener("click", () => {
+  snackbar.show("This is a snackbar");
 });
